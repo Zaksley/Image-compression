c82a3872121babdcabf11b82e196249aecaf07ab
WANG Charles <Charles.Wang@bordeaux-inp.fr>
2021-03-10 16:02:42 +0100

    Fix function household
    Signed-off-by: WANG Charles <Charles.Wang@bordeaux-inp.fr>

--- part1.py
+++ part1.py
@@ -1,26 +1,18 @@
 import numpy as np
 from math import *
 
-    # Return the matrice U = (X-Y) / || X - Y ||
-def get_U(X, Y):
-    
+# Algorithme de la transformation de Householder non optimisée
+# Prend X et Y de même taille.
+# Complexité de n**2
+def houseHolder(X,Y):
+    size = np.shape(X)[0] 
     U = X - Y
-    
-    norme = np.linalg.norm(U)
-    U = U / norme
-    return U
-
-def houseHolder(U):
-
-    size = np.shape(U)[1]
-    
-
-    H = np.identity(size) - (2 * np.dot(U, U.T))
-    
+    norme = np.linalg.norm(np.dot(U,U.T))
+    H = np.identity(size) - (2 * np.dot(U, U.T))/norme
     return H
 
-X = np.array( [[3, 4, 0]] )
-Y = np.array( [[0, 0, 5]] )
-
-
-print(houseHolder(get_U(X, Y)))
+if __name__=='__main__':
+    print("Test de la transformation de Householder non optimisée")
+    X = np.array( [[3], [4], [0]] )
+    Y = np.array( [[0], [0], [5]] )
+    print(houseHolder(X, Y))
