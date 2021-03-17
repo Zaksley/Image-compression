db6b7ac2314106126b7114790a2132da2b3e6fc6
Hector Piteau <hector.piteau@gmail.com>
2021-03-17 16:40:13 +0100

    Fix function bidiag
    Signed-off-by: Hector Piteau <hector.piteau@gmail.com>

--- part2.py
+++ part2.py
@@ -12,54 +12,54 @@ def bidiagonal_transformation(A,n,m):
     '''
     k = min(n,m)
     Qleft = np.eye(n)
-    BD = np.copy(A)
+    BD = np.array(A.copy(), dtype='float64')
     Qright = np.eye(m)
-    for i in range(k) :
+    for i in range(k) : # 0 ... k-1
         if i < m-1 :
             X = BD[i:,i]
             alpha = -np.sign(X[0])*np.linalg.norm(X)
-            Y = np.zeros(1 if i==0 else i)
+            Y = np.zeros(len(X))
             Y[0] = alpha
             H = houseHolder(X,Y)
             replace = np.dot(H,BD[i:,i:])
             BD[i:,i:] = replace
             Qleft[:,i:] = np.dot(Qleft[:,i:],H)
+        print(np.linalg.norm(A-np.dot(Qleft,np.dot(BD,Qright))))
         if i < n-2:
             X = BD[i,(i+1):]
             alpha = -np.sign(X[0])*np.linalg.norm(X)
-            Y = np.zeros(1 if i==0 else i)
+            Y = np.zeros(len(X))
             Y[0] = alpha
             H = houseHolder(X,Y)
             BD[i,(i+1):] = Y
             replace = np.dot(BD[(i+1):,(i+1):],H)
             BD[(i+1):,(i+1):] = replace
             Qright[(i+1):,:] = np.dot(H,Qright[(i+1):,:])
-        # print("===DEB===")
-        # print(Qleft)
-        # print("=========")
-        # print(BD)
-        # print("=========")
-        # print(Qright)
-        # print("===FIN===")
+        print(np.linalg.norm(A-np.dot(Qleft,np.dot(BD,Qright))))
+
     return (Qleft,BD,Qright)
 
 
-def tests():
+def testBidiagonal():
     A = np.array([[21,2,3],
                   [4, 6,9],
                   [3,11,32]])
     (Ql1,DB1,Qr1) = bidiagonal_transformation(A,3,3)
-    (Ql2,DB2,Qr2) = np.linalg.svd(A)
-    
+    print("Matrice Qleft :")
     print(Ql1)
-    # print(Ql2)
-    # print("=========")
+    print("Matrice Bidiagonale :")
     print(DB1)
-    # print(DB2)
-    # print("=========")
+    print("Matrice Qright :")
     print(Qr1)
-    # print(Qr2)
+    print("Matrice Qleft*BiDiag*Qright :")
     res = np.dot(Ql1,np.dot(DB1,Qr1));
     print(res)
+    estDiag = True
+    (n,m) = DB1.shape
+    epsilon = 10**(-14)
+    for i in range(n):
+        if(DB1[i,i] > -epsilon and DB1[i,i] < epsilon):
+            estDiag=False
+    print("Est-ce que la matrice BiDiag est bidiagonale ? " + str(estDiag))
 
-tests()
+testBidiagonal()
