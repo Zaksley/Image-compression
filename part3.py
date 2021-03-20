2a7bbbc17cb00256ecb5094f2771b8143e1adf18
MACHADO Dylan <Dylan.Machado@bordeaux-inp.fr>
2021-03-20 21:52:28 +0100

    Partie 3

--- part3.py
+++ part3.py
@@ -17,3 +17,46 @@ def SVD_Decomposition_BidiagonalMatrix(BD):
         V = np.dot(np.transpose(Q1),V)
         
     return (U,S,V)
+
+def QR_Transformation(BD):
+    '''
+        BD est la matrice bi-diagonale inférieure 
+    '''
+    (n,m) = BD.shape
+    R = A.copy()
+    Q = np.eye(n)
+
+    for i in range(min(m-2,n-1)):
+        
+        X = np.array([R[i][i],R[i+1][i]])
+        Y = np.zeros(2)
+        Y[0] = np.linalg.norm(X)
+    
+        H = houseHolder(X,Y)
+        R[i:i+2,i] = Y
+        
+        if (i < n-1):
+            R[i:i+2,i+1] = H*R[i:i+2,i+1]
+            
+        Q[:,i:i+2] = np.dot(Q[:,i:i+2],H)
+    
+    return (Q,R)
+
+
+def SVD_Decomposition_BidiagonalMatrix_Opti(BD):
+    '''
+        BD représente la matrice bi-diagonale
+    '''
+    (n,m) = BD.shape
+    S = BD.copy()
+    U = np.eye(n)
+    V = np.eye(m)
+    
+    for i in range(min(m-2,n-1)):
+        (Q1,R1) = QR_Transformation(np.transpose(S))
+        (Q2,R2) = QR_Transformation(np.transpose(R1))
+        S = R2
+        U = np.dot(U,Q2)
+        V = np.dot(np.transpose(Q1),V)
+        
+    return (U,S,V)
